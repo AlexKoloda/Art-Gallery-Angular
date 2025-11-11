ae0677ca1542a802f6558ccb72f78d2e23c17aee
Alex Koloda <xeniumsonyz@gmail.com>
2025-11-11 12:06:44 +0300

    Created shared component paper

---
+++ src/app/shared/paper/paper.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { ɵEmptyOutletComponent } from '@angular/router';
+
+@Component({
+  selector: 'app-paper',
+  imports: [],
+  templateUrl: './paper.html',
+  styleUrl: './paper.scss',
+})
+export class Paper {}
