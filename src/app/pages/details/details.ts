6513e15041a43522c0a62d80677c212fa028b0e8
Alex Koloda <xeniumsonyz@gmail.com>
2025-12-10 17:15:04 +0300

    Changed data for dto from api

--- src/app/pages/details/details.ts
+++ src/app/pages/details/details.ts
@@ -1,26 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { ArtDetails } from './art-details/art-details';
 import { IComment } from '../../models/comment.model';
 import { Comment } from '../../shared/comment/comment';
 import { CommentForm } from './comment-form/comment-form';
-
-//TODO: test comment array, replace with API data in feat.
-const TEST_COMMENT_ARRAY: IComment[] = [
-  { id: 1, value: 'Lorem ipsum dolor sit amet consectetur adipiscing elit quisque faucibus.' },
-  { id: 2, value: 'Lorem ipsum.' },
-  {
-    id: 3,
-    value:
-      'Lorem ipsum dolor sit amet consectetur adipiscing elit quisque faucibus ex sapien vitae pellentesque sem placerat in id cursus mi pretium tellus duis convallis tempus leo eu aenean sed diam urna tempor pulvinar vivamus fringilla lacus.',
-  },
-];
+import { Observable } from 'rxjs';
+import { CommentService } from '../../services/comment';
+import { AsyncPipe, isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-details',
-  imports: [ArtDetails, Comment, CommentForm],
+  imports: [ArtDetails, Comment, CommentForm, AsyncPipe],
   templateUrl: './details.html',
   styleUrl: './details.scss',
 })
-export class Details {
-  comments: IComment[] = TEST_COMMENT_ARRAY;
+export class Details implements OnInit {
+  artId: number = 0;
+  comments$: Observable<IComment[]>;
+
+  constructor(
+    private commentService: CommentService,
+    @Inject(PLATFORM_ID) private platformId: any
+  ) {
+    this.comments$ = commentService.fetchAllComments();
+  }
+
+  onCommentCreated() {
+    this.comments$ = this.commentService.fetchAllComments();
+  }
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      const art = history.state.art;
+      this.artId = art.id;
+    }
+  }
 }
