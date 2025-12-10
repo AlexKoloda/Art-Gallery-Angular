2de6514e3338da0a77f5342a6e919c039427c19d
Alex Koloda <xeniumsonyz@gmail.com>
2025-12-10 13:12:27 +0300

    Change component for dto from api

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -6,15 +6,40 @@ import { ButtonComponent } from '../../shared/button/button';
 
 import { Dialog } from '../../shared/dialog/dialog';
 import { AddArtForm } from './add-art-form/add-art-form';
+import { ArtService } from '../../services/art';
+import { Observable } from 'rxjs';
+import { IArt } from '../../models/art.model';
+import { AsyncPipe } from '@angular/common';
+import { ICategory } from '../../models/category.model';
+import { CategoryService } from '../../services/category';
+import { AddCategoryForm } from './add-category-form/add-category-form';
 
 @Component({
   selector: 'app-home',
-  imports: [Sidebar, ArtGrid, Pagination, ButtonComponent, Dialog, AddArtForm],
+  imports: [
+    Sidebar,
+    ArtGrid,
+    Pagination,
+    ButtonComponent,
+    Dialog,
+    AddArtForm,
+    AsyncPipe,
+    AddCategoryForm,
+  ],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home {
+  arts$: Observable<IArt[]>;
+  categories$: Observable<ICategory[]>;
+
+  constructor(private artService: ArtService, private categoryService: CategoryService) {
+    this.arts$ = this.artService.fetchAllArts();
+    this.categories$ = this.categoryService.fetchAllCategory();
+  }
+
   isAddArtModalOpen = false;
+  isAddCategoryModalOpen = false;
 
   openArtModal() {
     this.isAddArtModalOpen = true;
@@ -22,5 +47,18 @@ export class Home {
 
   closeArtModal() {
     this.isAddArtModalOpen = false;
+  }
+
+  openCategoryModal() {
+    this.isAddCategoryModalOpen = true;
+  }
+
+  closeCategoryModal() {
+    this.isAddCategoryModalOpen = false;
+  }
+
+  onCategoryCreated() {
+    this.categories$ = this.categoryService.fetchAllCategory();
+    this.closeCategoryModal();
   }
 }
