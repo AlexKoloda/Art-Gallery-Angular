cb8966f09e0e0b23e64e461dde7d9aea963853e1
Alex Koloda <xeniumsonyz@gmail.com>
2025-12-10 13:09:14 +0300

    Added form for create category at api

--- src/app/pages/home/add-category-form/add-category-form.ts
+++ src/app/pages/home/add-category-form/add-category-form.ts
@@ -1,6 +1,14 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { TextField } from '../../../shared/text-field/text-field';
 import { ButtonComponent } from '../../../shared/button/button';
+import { FormControl, Validators } from '@angular/forms';
+import { CategoryService } from '../../../services/category';
+import { ICategory } from '../../../models/category.model';
+
+export type TAddCategoryDto = {
+  name: string;
+  description: string;
+};
 
 @Component({
   selector: 'app-add-category-form',
@@ -10,6 +18,40 @@ import { ButtonComponent } from '../../../shared/button/button';
 })
 export class AddCategoryForm {
   @Output() cancel = new EventEmitter<void>();
+  @Output() created = new EventEmitter<ICategory>();
+
+  constructor(private categoryService: CategoryService) {}
+
+  categoryName = new FormControl<string>('', {
+    nonNullable: true,
+    validators: [Validators.required, Validators.minLength(2)],
+  });
+
+  categoryDescription = new FormControl<string>('', {
+    nonNullable: true,
+    validators: [Validators.required, Validators.minLength(2)],
+  });
+
+  onSubmit() {
+    if (this.categoryName.invalid || this.categoryDescription.invalid) {
+      this.categoryName.markAsTouched();
+      this.categoryDescription.markAsTouched();
+      return;
+    }
+
+    const formData: TAddCategoryDto = {
+      name: this.categoryName.value.trim(),
+      description: this.categoryDescription.value.trim(),
+    };
+
+    this.categoryService.postNewCategory(formData).subscribe({
+      next: (category) => {
+        this.created.emit(category);
+        this.cancel.emit();
+      },
+      error: (err) => console.error('Error', err),
+    });
+  }
 
   onClickClose() {
     this.cancel.emit();
