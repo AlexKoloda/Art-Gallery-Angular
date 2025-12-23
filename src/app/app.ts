56b9fafc1c706df78caf2507af4684b5f8f5bd02
Alex Koloda <xeniumsonyz@gmail.com>
2025-12-23 11:16:29 +0300

    Added loading component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,40 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {
+  NavigationCancel,
+  NavigationEnd,
+  NavigationError,
+  NavigationStart,
+  Router,
+  RouterOutlet,
+} from '@angular/router';
 import { Header } from './layout/header/header';
 import { Footer } from './layout/footer/footer';
 import { ToastContainer } from './shared/toast/toast-container/toast-container';
+import { Loading } from './shared/loading/loading/loading';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer, ToastContainer],
+  imports: [RouterOutlet, Header, Footer, ToastContainer, Loading],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('art-gallery');
+  isLoading = false;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationStart) {
+        this.isLoading = true;
+      }
+
+      if (
+        event instanceof NavigationEnd ||
+        event instanceof NavigationCancel ||
+        event instanceof NavigationError
+      ) {
+        this.isLoading = false;
+      }
+    });
+  }
 }
