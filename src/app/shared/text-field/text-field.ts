daa4d02a0af8d34e72faac083b32586fb098659a
Alex Koloda <xeniumsonyz@gmail.com>
2025-12-23 14:01:33 +0300

    Changed component for use reactive forms

--- src/app/shared/text-field/text-field.ts
+++ src/app/shared/text-field/text-field.ts
@@ -10,26 +10,21 @@ import { FormControl, ReactiveFormsModule } from '@angular/forms';
   styleUrls: ['./text-field.scss'],
 })
 export class TextField {
+  @Input({ required: true }) control!: FormControl;
+
   @Input() label = '';
   @Input() placeholder = '';
   @Input() type = 'text';
   @Input() variant: 'outlined' | 'filled' | 'standard' = 'outlined';
   @Input() size: 'small' | 'normal' = 'normal';
-  @Input() formControl?: FormControl;
+
   @Input() error = false;
   @Input() errorMessage = '';
   @Input() multiline = false;
   @Input() minRows = 1;
   @Input() maxRows?: number;
   @Input() fullWidth = false;
-  @Input() disable = false;
+
   @Input() select = false;
   @Input() options: { value: string | number; label: string }[] = [];
-
-  get control(): FormControl {
-    if (!this.formControl) {
-      this.formControl = new FormControl();
-    }
-    return this.formControl;
-  }
 }
